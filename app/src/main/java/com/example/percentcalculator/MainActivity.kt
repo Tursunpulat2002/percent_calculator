1abe817367ec11b5ff2b5c72bb5b8111c9086ff3
tursunpulat orolov <tursunpulat2002@gmail.com>
2022-12-25 20:11:30 -0500

    updated UI and fixed some errors

--- app/src/main/java/com/example/percentcalculator/MainActivity.kt
+++ app/src/main/java/com/example/percentcalculator/MainActivity.kt
@@ -5,46 +5,38 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.util.Log
 import com.example.percentcalculator.databinding.ActivityMainBinding
-import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    lateinit var shared : SharedPreferences
+    private lateinit var shared : SharedPreferences
 
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.e("Shamsi", "On Create")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.calcButtonId.setOnClickListener {
-            var price = binding.priceId.text.toString().toDoubleOrNull()
-            var percent = binding.percentageId.text.toString().toDoubleOrNull()
-            if(price != null && percent != null){
-                var outcome = price - (price*percent*.01)
-                binding.outcomeId.text = String.format("%.1f", outcome).toDouble().toString()
+            if(binding.priceId.text.toString().toDoubleOrNull() != null && binding.percentageId.text.toString().toDoubleOrNull() != null){
+                val price = binding.priceId.text.toString().toDoubleOrNull()!!
+                val percent = binding.percentageId.text.toString().toDoubleOrNull()!!
+                val outcome = price - (price*percent*.01)
+                binding.outcomeId.text = "You need to pay: $"+String.format("%.1f", outcome).toDouble().toString()
+            }else if(binding.priceId.text.toString().toDoubleOrNull() != null && binding.percentageId.text.toString().toDoubleOrNull() == null){
+                binding.outcomeId.text = "Please Enter Percent Off"
+            }else if(binding.priceId.text.toString().toDoubleOrNull() == null && binding.percentageId.text.toString().toDoubleOrNull() != null){
+                binding.outcomeId.text = "Please Enter Price of Item"
             }else{
-                binding.outcomeId.text = "Enter numbers"
+                binding.outcomeId.text = "Please Fill With Numbers"
             }
         }
-
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.e("Shamsi", "On Start")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.e("Shamsi", "On Resume")
         shared = getSharedPreferences("simpleDB" , Context.MODE_PRIVATE)
         val outcome = shared.getString("outcome", "Could not load").toString()
         val price = shared.getString("price", "Could not load").toString()
@@ -52,39 +44,21 @@ class MainActivity : AppCompatActivity() {
         if(outcome == "Could not load"){
             binding.priceId.setText("0")
             binding.percentageId.setText("0")
-            binding.outcomeId.setText("0")
+            binding.outcomeId.text = "0"
         }else{
             binding.priceId.setText(price)
             binding.percentageId.setText(percent)
-            binding.outcomeId.setText(outcome)
+            binding.outcomeId.text = outcome
         }
-
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.e("Shamsi", "On Restart")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.e("Shamsi", "On Pause")
         shared = getSharedPreferences("simpleDB" , Context.MODE_PRIVATE)
         val edit = shared.edit()
         edit.putString("outcome", binding.outcomeId.text.toString())
         edit.putString("price", binding.priceId.text.toString())
         edit.putString("percent", binding.percentageId.text.toString())
         edit.apply()
-        Log.e("Shamsi", binding.outcomeId.text.toString())
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.e("Shamsi", "On Stop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.e("Shamsi", "On Destroy")
     }
 }
